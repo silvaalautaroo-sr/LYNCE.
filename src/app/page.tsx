cf487f6611f3afc34d1b348522054280a33f97cf
silvaalautaroo-sr <silvaalautaroo@gmail.com>
2026-07-07 02:04:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
+import { routing } from "@/i18n/routing";
 
 export default function RootPage() {
-  redirect("/es");
+  redirect(`/${routing.defaultLocale}`);
 }
